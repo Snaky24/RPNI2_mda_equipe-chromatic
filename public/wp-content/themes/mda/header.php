3645ede574fb05a8ec9d50ceb0b56cf9ee4b7912
Yannick Blanchet <2440719@csfoy.ca>
2026-03-23 19:15:50 -0400

    Header terminer

--- public/wp-content/themes/mda/header.php
+++ public/wp-content/themes/mda/header.php
@@ -6,18 +6,26 @@
         <?php bloginfo('name');
         if (is_home() || is_front_page()) { ?>
             | <?php bloginfo('description');
-            } else { ?>
+        } else { ?>
             | <?php wp_title("", true);
-            } ?>
+        } ?>
     </title>
+
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/liaisons/css/styles.css">
+
     <?php wp_head(); ?>
 </head>
 
+<body <?php body_class(); ?>>
+
 <header class="entete">
+
+    <!-- TOPBAR  -->
     <div class="entete__topbar">
+
         <div class="entete__logo">
             <a href="<?php bloginfo('url'); ?>">
                 <img src="<?php echo get_template_directory_uri(); ?>/liaisons/images/logo_beige.png" alt="Logo">
@@ -35,19 +43,26 @@
         <div class="entete__recherche">
             <?php get_search_form(); ?>
         </div>
+
     </div>
 
-    <!-- Vidéo en dessous -->
+    <!-- VIDÉO -->
     <div class="entete__video">
         <video autoplay muted loop playsinline>
             <source src="<?php echo get_template_directory_uri(); ?>/liaisons/videos/entete__video.mp4" type="video/mp4">
         </video>
-        <div class="entete__titre">
-            <h1><?php the_title(); ?></h1>
+
+        <!-- OVERLAY -->
+        <div class="entete__contenu">
+            <h1 class="entete__slogan">
+                « Un lieu vivant où culture, patrimoine et communauté se rencontrent. »
+            </h1>
+
+            <a href="#" class="entete__btn">Voir plus</a>
         </div>
+
     </div>
 
 </header>
-
 
 <div class="contenu">
