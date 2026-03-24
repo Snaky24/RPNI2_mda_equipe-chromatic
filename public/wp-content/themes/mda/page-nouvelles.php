0e3ff24caf0ea025997ee678f8aa65f8eacb9306
MouhamedSyll22 <2434048@csfoy.ca>
2026-03-24 09:28:08 -0400

    Ajout du modèle de page "Nouvelles" avec styles SCSS associés et mise à jour des règles de réécriture dans .htaccess

--- public/wp-content/themes/mda/page-nouvelles.php
+++ public/wp-content/themes/mda/page-nouvelles.php
@@ -1,91 +1,59 @@
+    <?php
+    /* Template name: Nouvelles */
+    get_header();
+    ?>
+
+    <main class="page-nouvelles">
+      <section class="page-nouvelles__intro">
+        <h1 class="page-nouvelles__titre"><?php the_title(); ?></h1>
+        <div class="page-nouvelles__description">
+          <?php the_content(); ?>
+        </div>
+      </section>
+
       <?php
-        /* Template name: Nouvelles */
-        get_header(); //Appel de l'inclusion d'entête de page
-        echo "page-nouvelles.php";
-        ?>
+      $requete_nouvelles = new WP_Query(array(
+        'post_type' => 'nouvelles',
+        'posts_per_page' => -1,
+        'post_status' => 'publish',
+        'orderby' => 'date',
+        'order' => 'DESC',
+      ));
+      ?>
 
-      <main class="page">
+      <section class="page-nouvelles__liste">
+        <?php if ($requete_nouvelles->have_posts()) { ?>
+          <div class="nouvelles-grid">
+            <?php while ($requete_nouvelles->have_posts()) {
+              $requete_nouvelles->the_post(); ?>
+              <article class="nouvelle-carte">
+                <a class="nouvelle-carte__image-lien" href="<?php the_permalink(); ?>">
+                  <?php if (has_post_thumbnail()) {
+                    the_post_thumbnail('image-single', array(
+                      'class' => 'nouvelle-carte__image',
+                      'loading' => 'lazy',
+                    ));
+                  } else { ?>
+                    <span class="nouvelle-carte__image--placeholder" aria-hidden="true"></span>
+                  <?php } ?>
+                </a>
 
-          <?php //var_dump($post); //Ce que reçoit la page
-            ?>
-
-          <div>
-              <h2><?php the_title() //fonction native WP
-                    ?></h2>
+                <div class="nouvelle-carte__contenu">
+                  <p class="nouvelle-carte__date"><?php echo get_the_date('j F Y'); ?></p>
+                  <h2 class="nouvelle-carte__titre">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                  </h2>
+                  <p class="nouvelle-carte__extrait"><?php echo wp_trim_words(get_the_excerpt(), 28); ?></p>
+                  <a class="nouvelle-carte__btn" href="<?php the_permalink(); ?>">Lire la nouvelle</a>
+                </div>
+              </article>
+            <?php } ?>
           </div>
-          <?php
-            // Utiliser le code ci-dessous pour créer une image responsive
-            if (has_post_thumbnail()) {
-                $sizes = array();
-                $sizes[0] = wp_get_attachment_image_src(get_post_thumbnail_id(), "large");
-                $sizes[1] = wp_get_attachment_image_src(get_post_thumbnail_id(), "medium");
-                $sizes[2] = wp_get_attachment_image_src(get_post_thumbnail_id(), "thumbnail"); ?>
+          <?php wp_reset_postdata(); ?>
+        <?php } else { ?>
+          <p class="page-nouvelles__vide">Aucune nouvelle n'est disponible pour le moment.</p>
+        <?php } ?>
+      </section>
+    </main>
 
-              <picture>
-                  <source media="(min-width: 801px)" srcset="<?php echo $sizes[0][0]; ?>">
-                  <source media="(min-width: 601px)" srcset="<?php echo $sizes[1][0]; ?>">
-                  <img src="<?php echo $sizes[2][0]; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>">
-              </picture>
-          <?php } ?>
-          <p>
-              <?php the_content() ?>
-              <?php  //echo $post->post_content; 
-                ?>
-          </p>
-          <?php
-            //Requête et boucle d'affichage des articles avec ACF
-            //À mettre dans les pages utilisant les articles personnalisés et adapter****************
-            $posts = get_posts(array(
-                'posts_per_page' => -1,
-                'post_type'    => 'nouvelles',
-                'post_status' => 'publish',
-                'orderby' => 'the_title',
-                'order' => 'ASC',
-            ));
-
-            // var_dump($posts);
-
-            if (have_posts()) {
-                //tant qu'il restera des articles
-                foreach ($posts as $post) { ?>
-                  <article class="article">
-                      <header class="article__entete">
-                          <h2 class="article__titre">
-                              <?php //affiche le lien et le titre de l'article'
-                                ?>
-                              <a class="article__lien" href="<?php the_permalink(); ?>"><?php the_title() ?></a>
-                          </h2>
-                      </header>
-                      <?php
-
-                        $image_info = get_field("photo_1");
-
-                        //Si l'image est définie dans ACF
-                        if ($image_info != null) {
-
-                            //Utiliser la balise picture pour le redimensionnement de l'image 
-                        ?>
-                          <picture>
-                              <source media="(min-width: 800px)" srcset="<?php echo $image_info['sizes']["large"]; ?>">
-                              <source media="(min-width: 601px)" srcset="<?php echo $image_info['sizes']["medium"]; ?>">
-                              <img src="<?php echo $image_info['sizes']['thumbnail']; ?>" alt="<?php echo $image_info["alt"]; ?>">
-                          </picture>
-
-                      <?php } ?>
-
-                      <p><?php echo get_field("nom_membre") ?></p>
-
-                      <p class="article__texte">
-                          <?php //affiche le l'extrait de la réalisation
-                            the_excerpt();
-                            ?>
-                      </p>
-                  </article>
-          <?php }
-
-                //réinitialise les données reçues par défaut du gabarit pour afficher le
-                //reste des informations de la page, s'il y a lieu
-                //wp_reset_postdata();
-            } ?>
-      </main>
-      <?php get_footer() ?>
+    <?php get_footer(); ?>
