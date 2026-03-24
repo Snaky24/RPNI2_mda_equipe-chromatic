09da4817aea32b060a788f61441f5fb1247ddf45
Yannick Blanchet <2440719@csfoy.ca>
2026-03-24 08:47:47 -0400

    début style accueil et correction navigation-admin

--- public/wp-content/themes/mda/footer.php
+++ public/wp-content/themes/mda/footer.php
@@ -41,3 +41,5 @@
             
         </div>
     </footer>
+    <?php wp_footer(); ?>
+    
--- public/wp-content/themes/mda/home.php
+++ public/wp-content/themes/mda/home.php
@@ -1,38 +1,78 @@
 <?php get_header(); ?>
-<main class="page">
-    <?php echo "home.php"; ?>
-    <?php
-    if (have_posts()) {
-        while (have_posts()) {
-            the_post(); ?>
-            <article class="article">
-                <header class="article__entete">
-                    <h2 class="article__titre">
-                        <?php ?>
-                        <a class="article__lien" href="<?php the_permalink(); ?>"><?php the_title() ?></a>
-                    </h2>
-                </header>
-                <?php
-                // Utiliser le code ci-dessous pour créer une image responsive
-                if (has_post_thumbnail()) {
-                    $sizes = array();
-                    $sizes[0] = wp_get_attachment_image_src(get_post_thumbnail_id(), "large");
-                    $sizes[1] = wp_get_attachment_image_src(get_post_thumbnail_id(), "medium");
-                    $sizes[2] = wp_get_attachment_image_src(get_post_thumbnail_id(), "thumbnail"); ?>
 
-                    <picture>
-                        <source media="(min-width: 801px)" srcset="<?php echo $sizes[0][0]; ?>">
-                        <source media="(min-width: 601px)" srcset="<?php echo $sizes[1][0]; ?>">
-                        <img src="<?php echo $sizes[2][0]; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>">
-                    </picture>
-                <?php } ?>
-                <p class="article__texte">
-                    <?php
-                    the_content();
-                    ?>
-                </p>
-            </article>
-    <?php }
-    } ?>
+<main class="site-main">
+    <h1 class="titre-principal">La Maison des arts de Saint-Augustin-de-Desmaures</h1>
+
+    <section class="nouvelles">
+        <h2 class="section__titre">Nouvelles</h2>
+        <div class="nouvelles__grille">
+            <?php 
+            $query_news = new WP_Query(array(
+                'category_name' => 'nouvelles', 
+                'posts_per_page' => 3
+            ));
+            if ($query_news->have_posts()) : while ($query_news->have_posts()) : $query_news->the_post(); ?>
+                <article class="carte-polaroid">
+                    <div class="polaroid__image">
+                        <?php if(has_post_thumbnail()) : the_post_thumbnail('medium'); endif; ?>
+                    </div>
+                    <div class="polaroid__texte">
+                        <p><?php echo wp_trim_words(get_the_excerpt(), 12); ?></p>
+                        <a href="<?php the_permalink(); ?>" class="btn-voir-plus">Voir plus</a>
+                    </div>
+                </article>
+            <?php endwhile; wp_reset_postdata(); endif; ?>
+        </div>
+    </section>
+
+    <section class="a-propos">
+        <div class="a-propos__contenu">
+            <div class="a-propos__texte">
+                <h2>À propos de nous</h2>
+                <p>La Maison des arts de Saint-Augustin-de-Desmaures redonne vie au presbytère du village en le transformant en un lieu culturel accessible et rassembleur. Notre mission : offrir une programmation professionnelle en arts visuels, arts vivants, littérature et cinéma, accueillir des résidences d'artistes et développer des projets de médiation culturelle avec les écoles et les organismes du territoire. Ancré dans une approche durable et respectueuse du patrimoine, le projet crée un espace chaleureux où la communauté peut se rencontrer, s'inspirer et célébrer la création.</p>
+            </div>
+            <div class="a-propos__images">
+                <img src="<?php echo get_template_directory_uri(); ?>/liaisons/images/presbytere-1.jpg" class="img-inclinee-1">
+                <img src="<?php echo get_template_directory_uri(); ?>/liaisons/images/presbytere-2.jpg" class="img-inclinee-2">
+            </div>
+        </div>
+    </section>
+
+    <section class="volet-culturel">
+        <h2 class="section__titre">Volet culturel</h2>
+        <?php 
+        $query_volet = new WP_Query(array(
+            'category_name' => 'volets', 
+            'posts_per_page' => 1 
+        ));
+        if ($query_volet->have_posts()) : while ($query_volet->have_posts()) : $query_volet->the_post(); ?>
+            <div class="volet-culturel__conteneur">
+                <div class="volet-culturel__images">
+                    <div class="img-polaroid img-polaroid--base">
+                        <?php the_post_thumbnail('large'); ?>
+                    </div>
+                    <div class="img-polaroid img-polaroid--dessus">
+                        <img src="<?php echo get_template_directory_uri(); ?>/liaisons/images/photo-2.jpg">
+                    </div>
+                </div>
+
+                <div class="volet-culturel__bloc-info">
+                    <h2><?php the_title(); ?></h2>
+                    <p><?php echo get_the_excerpt(); ?></p>
+                    <a href="<?php the_permalink(); ?>" class="btn-beige">En apprendre plus</a>
+                </div>
+            </div>
+        <?php endwhile; wp_reset_postdata(); endif; ?>
+
+        <div class="volet-culturel__controles">
+            <span class="fleche">&lt;</span>
+            <span class="point"></span>
+            <span class="point point--actif"></span>
+            <span class="point"></span>
+            <span class="point"></span>
+            <span class="fleche">&gt;</span>
+        </div>
+    </section>
 </main>
+
 <?php get_footer(); ?>
