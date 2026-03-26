e5f0b74376b2a42d2f4087609ba493c55fbd4c0e
Camille <kultureasian@gmail.com>
2026-03-26 15:45:10 -0400

    Médiation

--- public/wp-content/themes/mda/single-voletsculturels.php
+++ public/wp-content/themes/mda/single-voletsculturels.php
@@ -1,59 +1,78 @@
 <?php
 get_header();
-echo "single-voletsculturels.php";
 ?>
 
-<main class="page">
+<main class="page-voletsculturels">
+    <div class="contenu-page">
+        <?php the_content(); ?>
+    </div>
+    <?php
+    // Utiliser le code ci-dessous pour créer une image responsive
+    if (has_post_thumbnail()) {
+        $sizes = array();
+        $sizes[0] = wp_get_attachment_image_src(get_post_thumbnail_id(), "large");
+        $sizes[1] = wp_get_attachment_image_src(get_post_thumbnail_id(), "medium");
+        $sizes[2] = wp_get_attachment_image_src(get_post_thumbnail_id(), "thumbnail"); ?>
+        <picture>
+            <source media="(min-width: 801px)" srcset="<?php echo $sizes[0][0]; ?>">
+            <source media="(min-width: 601px)" srcset="<?php echo $sizes[1][0]; ?>">
+            <img src="<?php echo $sizes[2][0]; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>">
+        </picture>
+    <?php } ?>
 
-    <?php the_post(); //nécessaire à the_author() et the_date()
-    // var_dump($post); //Ce que reçoit la page
+    <?php
+    //Requête et boucle d'affichage des articles avec ACF
+    //À mettre dans les pages utilisant les articles personnalisés et adapter****************
+    $posts = get_posts(array(
+        'posts_per_page' => -1,
+        'post_type'    => 'voletsculturels',
+        'post_status' => 'publish',
+        'orderby' => 'the_title',
+        'order' => 'ASC',
+    ));
     ?>
-    <article class="article">
-        <header class="article__entete">
-            <h2 class="article__titre"><?php the_title() ?></h2>
-        </header>
 
+    <?php if (have_posts()) { ?>
+        <div class="articles-grid">
+            <?php foreach ($posts as $post) { ?>
+                <article class="article-volets">
+                    <?php
 
+                    $image_info = get_field("photo_1");
 
-        <?php
+                    //Si l'image est définie dans ACF
+                    if ($image_info != null) {
 
-        //Affichage de plusieurs images responsives définies dans les champs d'ACF
+                        //Utiliser la balise picture pour le redimensionnement de l'image 
+                    ?>
+                        <picture class="picture-nom_volet-volets">
+                            <source media="(min-width: 800px)" srcset="<?php echo $image_info['sizes']["large"]; ?>">
+                            <source media="(min-width: 601px)" srcset="<?php echo $image_info['sizes']["medium"]; ?>">
+                            <img src="<?php echo $image_info['sizes']['thumbnail']; ?>" alt="<?php echo $image_info["alt"]; ?>">
+                        </picture>
 
-        //Boucler dans les champs d'images de ACF - un champ par image
-        //Nommer les champs avec un numéro - photo_1, photo_2
-        //Ici 8 images possibles
-        for ($cpt = 1; $cpt <= 8; $cpt++) {
-
-            //Le champ d'ACF doit être configuré pour retourner un tableau
-            $image_info = get_field("photo_" . $cpt);
-
-            //Si l'image est définie dans ACF
-            if ($image_info != null) {
-                //Utiliser la balise picture pour le redimensionnement de l'image
-                //ici on utilise trois images personnalisées... Mais on peut utiliser thumbnail, medium, etc...
-                //l'attribut sizes contient tout les formats d'image nécessaire.
-        ?>
-                <picture>
-                    <source media="(min-width: 800px)" srcset="<?= $image_info['sizes']["large"] ?>">
-                    <source media="(min-width: 601px)" srcset="<?= $image_info['sizes']["medium"] ?>">
-                    <img src="<?= $image_info['sizes']["thumbnail"] ?>" alt="<?= $image_info["alt"]; ?>">
-                </picture>
+                    <?php } ?>
+                    <div class="article__fond-volets">
+                        <header class="article__entete-volets">
+                            <h2 class="article__titre-volets">
+                                <?php //affiche le lien et le titre de l'article'
+                                ?>
+                                <p class="article__lien-volets"><?php the_title() ?></p>
+                            </h2>
+                        </header>
+                        <div class="article__btn-volets">
+                            <a class="btn__a-volets" href="<?php the_permalink(); ?>">
+                                <button class="article__a-volets">En savoir plus</button>
+                            </a>
+                        </div>
+                    </div>
+                </article>
         <?php }
+
+            //réinitialise les données reçues par défaut du gabarit pour afficher le
+            //reste des informations de la page, s'il y a lieu
+            //wp_reset_postdata();
         } ?>
-
-
-
-        <p class="article__texte">
-            <?php the_content() ?>
-        </p>
-        <footer class="article__piedPage">
-            <h4>Par: <?php the_author(); //Attention! Nécessite un appel à the_post() avant cet affichage 
-                        ?></h4>
-            <h4> Publié le: <?php the_date(); //Attention! Nécessite un appel à the_post() avant cet affichage 
-                            ?></h4>
-        </footer>
-    </article>
-
+        </div>
 </main>
-
 <?php get_footer() ?>
