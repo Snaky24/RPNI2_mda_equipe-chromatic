45c8c2ac46123a52c452862326897cccddb017c6
Camille <kultureasian@gmail.com>
2026-03-26 15:25:40 -0400

    Polaroid

--- public/wp-content/themes/mda/page-voletsculturels.php
+++ public/wp-content/themes/mda/page-voletsculturels.php
@@ -14,13 +14,13 @@ get_header(); //Appel de l'inclusion d'entête de page
         $sizes[0] = wp_get_attachment_image_src(get_post_thumbnail_id(), "large");
         $sizes[1] = wp_get_attachment_image_src(get_post_thumbnail_id(), "medium");
         $sizes[2] = wp_get_attachment_image_src(get_post_thumbnail_id(), "thumbnail"); ?>
-        <picture>
-            <source media="(min-width: 801px)" srcset="<?php echo $sizes[0][0]; ?>">
-            <source media="(min-width: 601px)" srcset="<?php echo $sizes[1][0]; ?>">
-            <div class="volets-top__images">
+        <div class="img-volet">
+            <picture>
+                <source media="(min-width: 801px)" srcset="<?php echo $sizes[0][0]; ?>">
+                <source media="(min-width: 601px)" srcset="<?php echo $sizes[1][0]; ?>">
                 <img src="<?php echo $sizes[2][0]; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>">
-            </div>
-        </picture>
+            </picture>
+        </div>
     <?php } ?>
 
     <?php
