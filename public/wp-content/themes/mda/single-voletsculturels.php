40a54520d85e69c7aaacd4842c567091be6db325
Camille <kultureasian@gmail.com>
2026-03-28 15:06:46 -0400

    Images

--- public/wp-content/themes/mda/single-voletsculturels.php
+++ public/wp-content/themes/mda/single-voletsculturels.php
@@ -1,12 +1,8 @@
 <?php get_header(); ?>
 
 <main class="page">
-    <div class="volets-fond">
-        <div class="titre-paragraphe">
-            <h2 class="volets__titre"><?php the_title() ?></h2>
-            <p class="volets__paragraphe"><?php the_content() ?></p>
-        </div>
-        <div class="volets_images">
+    <section class="section">
+        <div class="images">
             <?php
 
             $image_info = get_field("photo_1");
@@ -16,34 +12,20 @@
 
                 //Utiliser la balise picture pour le redimensionnement de l'image 
             ?>
-                <picture class="volets_images-rotate1">
+                <picture class="picture-volet1">
                     <source media="(min-width: 800px)" srcset="<?php echo $image_info['sizes']["large"]; ?>">
                     <source media="(min-width: 601px)" srcset="<?php echo $image_info['sizes']["medium"]; ?>">
-                    <img class="volets_images-img1" src="<?php echo $image_info['sizes']['thumbnail']; ?>" alt="<?php echo $image_info["alt"]; ?>">
+                    <img src="<?php echo $image_info['sizes']['thumbnail']; ?>" alt="<?php echo $image_info["alt"]; ?>">
                 </picture>
 
             <?php } ?>
+            <img src="etage-lumineux.jpg" alt="Étage lumineux" class="photo photo-2">
         </div>
-        <div class="volets_images">
-            <?php
 
-            $image_info = get_field("photo_2");
-
-            //Si l'image est définie dans ACF
-            if ($image_info != null) {
-
-                //Utiliser la balise picture pour le redimensionnement de l'image 
-            ?>
-                <picture class="volets_images-rotate2">
-                    <source media="(min-width: 800px)" srcset="<?php echo $image_info['sizes']["large"]; ?>">
-                    <source media="(min-width: 601px)" srcset="<?php echo $image_info['sizes']["medium"]; ?>">
-                    <img class="volets_images-img2" src="<?php echo $image_info['sizes']['thumbnail']; ?>" alt="<?php echo $image_info["alt"]; ?>">
-                </picture>
-
-            <?php } ?>
+        <div class="texte">
+            <?php the_content() ?>
         </div>
-        <?php the_content() ?>
-    </div>
+    </section>
 </main>
 
 <?php get_footer(); ?>
